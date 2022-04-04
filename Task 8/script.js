b1668697539fefc05b6607829fed508c81ef38a3
ed <allgoodata@outlook.com>
2022-04-04 11:14:21 +0300

    Task8 completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,18 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = function (num1, num2) {
+    return num1 + num2;
+  };
+  this.subtraction = function (num1, num2) {
+    return num1 - num2;
+  };
+  this.multiplication = function (num1, num2) {
+    return num1 * num2;
+  };
+  this.division = function (num1, num2) {
+    return num1 / num2;
+  };
+}
