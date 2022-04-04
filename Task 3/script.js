34ceba0301af74bfb0728a9b1c93315959f963e8
ed <allgoodata@outlook.com>
2022-04-04 09:51:06 +0300

    Task3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,41 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+async function getUsers() {
+  const request = await fetch(ENDPOINT);
+  const responseData = await request.json();
+  return responseData;
+}
+
+function userCard(userObj) {
+  const { login, avatar_url } = userObj;
+  return `
+<div class="userCard-container">
+  <div class=userImg-container>
+    <img src=${avatar_url} alt="avatar" />
+  </div>
+  <h2>${login}</h2>
+</div>`;
+}
+
+function paintToDOM(users) {
+  const outputContainer = document.getElementById("output");
+  outputContainer.innerHTML = "";
+  users.forEach((element) => {
+    outputContainer.innerHTML += userCard(element);
+  });
+}
+
+function listenToButton() {
+  const btn = document.getElementById("btn");
+  btn.addEventListener("click", () => {
+    getUsers().then((data) => {
+      paintToDOM(data);
+    });
+  });
+}
+
+// CALL STACK
+listenToButton();
